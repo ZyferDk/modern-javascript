58ca0679444e7d20f3aa34d8a0778d5b8942e9b8
surajidk <surajidk12@gmail.com>
2021-03-17 16:33:55 +0700

    add question number 5

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -1,20 +1,20 @@
-const correctAnswers = ['B', 'B', 'B', 'B'];
+const correctAnswers = ['B', 'A', 'B', 'B', 'A'];
 const form = document.querySelector('.quiz-form');
 
 form.addEventListener('submit', e => {
-  e.preventDefault();
+    e.preventDefault();
 
-  let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
 
-  // check the answers
-  userAnswers.forEach((answer, index) => {
-    if (answer === correctAnswers[index]){
-      score += 25;
-    }
-  });
+    // check the answers
+    userAnswers.forEach((answer, i) => {
+        if (answer === correctAnswers[i]) {
+            score += 20;
+        }
+    });
 
-  // log the score to console
-  console.log(score);
+    // log the score to console
+    console.log(score);
 
 });
